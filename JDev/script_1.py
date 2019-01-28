f89d9437b8a92ba676027ba4f5831d00e4021708
unknown <jotanice>
2019-01-28 11:25:19 -0200

    Upload do script_1.py para o Github.

---
+++ JDev/script_1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+nome = input ("Digite seu nome: "+"\nOu Digite Limpa para apagar o conteúdo do arquivo.")
+
+if nome == "limpa" or nome == "Limpa":
+    A = open ("arq2.txt", "w")
+    A.close ()
+
+else:
+
+    v1_str = input ("Digite o valor de x: ")
+    v2_str = input ("Digite o valor de y: ")
+
+    v1_int = int(v1_str)
+    v2_int = int(v2_str)
+
+
+    def sum(x, y):
+        return x + y
+    s = sum(v1_int, v2_int)
+    def multiply(x, y):
+        return x * y
+    m = multiply(v1_int, v2_int)
+
+    print (nome)
+
+
+    if nome == "Joao":
+            print (sum(s,m))
+            s_str = str(s)
+            A = open ("arq2.txt", "a")
+            A.write (v1_str+"\n"+v2_str+"\n"+nome+"\n"+s_str+"\n\n")
+            A.close ()
+    else:
+            print (m)
+            m_str = str(m)
+            A = open ("arq2.txt", "a")
+            A.write (v1_str+"\n"+v2_str+"\n"+nome+"\n"+m_str+"\n\n")
+            A.close ()
