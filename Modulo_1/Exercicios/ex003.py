34d149a566f7a86d2349df152b9e2e38f3fdb4ba
Jotanice <joaofabianosiqueira@gmail.com>
2019-09-15 00:02:15 -0300

    ex003

--- Modulo_1/Exercicios/ex003.py
+++ Modulo_1/Exercicios/ex003.py
@@ -2,4 +2,5 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 r = n1 + n2
-print('O resultado da soma entre {}!'.format(n1).format(n2).format(r))
+#print('A soma entre ', str(n1), ' e ', str(n2), ' é ', str(r), '!')
+print('A soma entre {} e {} é {}.'.format(n1, n2, r))
